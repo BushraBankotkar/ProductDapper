3c157f8ecdce382c3999e0f34a368174ab4bdcc3
Bushra <bushrabankotkar@gmail.com>
2023-06-20 15:32:32 +0530

    Adding appsettings.hson, ProdDb.sql

---
+++ ProductDapper/Data/ProdDb.sql
@@ -0,0 +1,22 @@
+USE [ProductDb]
+GO
+
+/****** Object:  Table [dbo].[Products]    Script Date: 20-06-2023 13:13:43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Products](
+	[ProductID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductName] [varchar](100) NULL,
+	[ProductPrice] [decimal](10, 2) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
